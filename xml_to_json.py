9fcf1d1d3b6b2160a4e295646d784e5d6dabf2f5
unknown <amir.akhunov@gmail.com>
2022-10-31 22:33:32 +0300

    first commit

--- xml_to_json.py
+++ xml_to_json.py
@@ -1,11 +1,12 @@
 def main(s):
-    if "<" not in s or ">" not in s: return s
+    if ("<" or ">") not in s: return s
 
     tag_name = get_tag_name(s)
     open_tag = f"<{tag_name}>"
     close_tag = f"</{tag_name}>" 
     
     print(s)
+
     if s.find(close_tag) + len(close_tag) == len(s):
         return {tag_name: main(s[s.find(open_tag) + len(open_tag):s.find(close_tag)])}
     return {tag_name: main(s[s.find(open_tag) + len(open_tag):s.find(close_tag)]), get_tag_name(s[s.find(close_tag) + len(close_tag):]): main(s[s.find(close_tag) + len(close_tag):])}
